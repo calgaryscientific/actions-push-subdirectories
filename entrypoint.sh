ab1cf03c9143c7e1621cc476bdada18165d09468
mjs <stephure@users.noreply.github.com>
2020-07-24 22:00:43 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,53 +3,28 @@
 set -e
 
 FOLDER=$1
-GITHUB_USERNAME=$2
-STARTER_NAME="${3:-name}"
+TARGET_BRANCH=$2
+TARGET_REPO=$3
+GITHUB_USERNAME=github-actions
 BASE=$(pwd)
 
-git config --global user.email "johno-actions-push-subdirectories@example.org"
+git config --global user.email "github-actions-bot@github.com"
 git config --global user.name "$GITHUB_USERNAME"
 
-echo "Cloning folders in $FOLDER and pushing to $GITHUB_USERNAME"
-echo "Using $STARTER_NAME as the package.json key"
+# clone, delete files in the clone, and copy (new) files over
+# this handles file deletions, additions, and changes seamlessly
+CLONE_DIR="__${FOLDER}__clone__"
 
-# sync to read-only clones
-for folder in $FOLDER/*; do
-  [ -d "$folder" ] || continue # only directories
-  cd $BASE
+git clone --depth 1 https://$API_TOKEN_GITHUB@github.com/$TARGET_REPO.git  $CLONE_DIR &> /dev/null
+cd $CLONE_DIR
+find . | grep -v ".git" | grep -v "^\.*$" | xargs rm -rf # delete all files (to handle deletions in monorepo)
+cp -r $BASE/$folder/. .
 
-  echo "$folder"
-
-  NAME=$(cat $folder/package.json | jq --arg name "$STARTER_NAME" -r '.[$name]')
-  echo "  Name: $NAME"
-  IS_WORKSPACE=$(cat $folder/package.json | jq -r '.workspaces')
-  CLONE_DIR="__${NAME}__clone__"
-  echo "  Clone dir: $CLONE_DIR"
-
-  # clone, delete files in the clone, and copy (new) files over
-  # this handles file deletions, additions, and changes seamlessly
-  git clone --depth 1 https://$API_TOKEN_GITHUB@github.com/$GITHUB_USERNAME/$NAME.git $CLONE_DIR &> /dev/null
-  cd $CLONE_DIR
-  find . | grep -v ".git" | grep -v "^\.*$" | xargs rm -rf # delete all files (to handle deletions in monorepo)
-  cp -r $BASE/$folder/. .
-
-  # generate a new yarn.lock file based on package-lock.json unless you're in a workspace
-  if [ "$IS_WORKSPACE" = null ]; then
-    echo "  Regenerating yarn.lock"
-    rm -rf yarn.lock
-    yarn
-  fi
-
-  # Commit if there is anything to
-  if [ -n "$(git status --porcelain)" ]; then
-    echo  "  Committing $NAME to $GITHUB_REPOSITORY"
-    git add .
-    git commit --message "Update $NAME from $GITHUB_REPOSITORY"
-    git push origin master
-    echo  "  Completed $NAME"
-  else
-    echo "  No changes, skipping $NAME"
-  fi
-
-  cd $BASE
-done
+# Commit if there is anything to
+if [ -n "$(git status --porcelain)" ]; then
+  git add .
+  git commit --message "Update $TARGET_REPO from $GITHUB_REPOSITORY"
+  git push origin develop
+else
+  echo "  No changes, skipping update"
+fi
